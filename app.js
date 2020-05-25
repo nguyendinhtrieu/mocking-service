7ff5008bdd7fd237161bb0139c68538f22993514
Nguyen Dinh Trieu <nguyen.dinh.trieu@sun-asterisk.com>
2020-05-25 10:40:39 +0700

    Init mocking service

---
+++ app.js
@@ -0,0 +1,36 @@
+var express = require('express');
+
+var app = express();
+
+const conversations = require('./files/conversation.json')
+app.get('/conversation', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.send(conversations)
+});
+
+
+const detailComment = require('./files/detail-comment.json')
+app.get('/conversation/detail/108241560890445_108264224221512', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.send(detailComment)
+});
+
+const detailInbox1 = require('./files/detail-inbox1.json')
+app.get('/conversation/detail/t_1391335917734629', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.send(detailInbox1)
+});
+
+const detailInbox2 = require('./files/detail-inbox2.json')
+app.get('/conversation/detail/t_263190571497633', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.send(detailInbox2)
+});
+
+app.listen(1111, () => {
+    console.log('Application running on http://localhost:1111');
+});
